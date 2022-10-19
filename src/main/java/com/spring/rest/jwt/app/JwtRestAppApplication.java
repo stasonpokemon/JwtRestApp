c8b11e7df9a46ffd2344c35003fde408c90df748
Stanislau Trebnikau <Stanislau_Trebnikau@epam.com>
2022-10-20 00:22:36 +0300

    Initial application properties

---
+++ src/main/java/com/spring/rest/jwt/app/JwtRestAppApplication.java
@@ -0,0 +1,13 @@
+package com.spring.rest.jwt.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class JwtRestAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(JwtRestAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/rest/jwt/app/JwtRestAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.rest.jwt.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JwtRestAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
